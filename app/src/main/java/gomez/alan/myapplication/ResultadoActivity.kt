2739ea39e7dc4b8d2b3487e2f6430a136468e847
AlanDevory <alan.gortega@alumnos.udg.mx>
2022-03-31 01:04:49 -0600

    Initial commit

---
+++ app/src/main/java/gomez/alan/myapplication/ResultadoActivity.kt
@@ -0,0 +1,27 @@
+package gomez.alan.myapplication
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
+
+class ResultadoActivity : AppCompatActivity() {
+    lateinit var animView:LottieAnimationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+
+        animView = findViewById(R.id.animation_view)
+
+        val extras = intent.extras
+        val respuestaCorrecta = extras!!.getBoolean("respuesta")
+        if(respuestaCorrecta){
+            animView.setAnimation(R.raw.correctanimation)
+        }else{
+            animView.setAnimation(R.raw.sadface)
+        }
+
+    }
+
+}
